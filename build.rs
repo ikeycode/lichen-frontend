08689e7191c657e4cff3c091093cb244bfcf5099
Ikey Doherty <ikey@serpentos.com>
2025-01-31 10:12:09 +0000

    ui: Responsive for dark vs light mode
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- build.rs
+++ build.rs
@@ -3,7 +3,7 @@
 // SPDX-License-Identifier: MPL-2.0
 
 fn main() {
-    let config = slint_build::CompilerConfiguration::new().with_style("fluent-dark".to_owned());
+    let config = slint_build::CompilerConfiguration::new().with_style("fluent".to_owned());
     slint_build::compile_with_config("ui/index.slint", config)
         .expect("Failed to compile UI assets");
 }
